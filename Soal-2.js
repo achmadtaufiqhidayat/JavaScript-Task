b41210b687c2ef2058a742e64cfe6b076e6b472f
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2023-03-31 13:41:32 +0700

    soal 2 done

---
+++ Soal-2.js
@@ -0,0 +1,10 @@
+
+
+for(a=0; a<= 10; a++){
+    // kondisi disini saya coment, jika condisi aktif maka perulangan yang ditampilkan akan dimulai dari perulangan ke 1
+    // if(a ===0){
+    //     continue;
+    // }
+    let nilai = a * 2;
+    console.log('Ini hasil perulangan ke ' + a +" : "+nilai);
+}
